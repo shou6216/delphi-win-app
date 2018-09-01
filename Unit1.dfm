2379faa35d367237c0318f4c3a911e6dd421bfb5
s-yoshino <shou-y6216@yoshinoda.com>
2018-09-02 00:14:39 +0900

    [update] onclick event

--- Unit1.dfm
+++ Unit1.dfm
@@ -13,4 +13,29 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Edit1: TEdit
+    Left = 8
+    Top = 8
+    Width = 121
+    Height = 25
+    TabOrder = 0
+    Text = 'Edit1'
+  end
+  object Button1: TButton
+    Left = 128
+    Top = 8
+    Width = 75
+    Height = 25
+    Caption = #23455#34892
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object ListBox1: TListBox
+    Left = 8
+    Top = 48
+    Width = 195
+    Height = 97
+    ItemHeight = 13
+    TabOrder = 2
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,14 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Edit1: TEdit;
+    Button1: TButton;
+    ListBox1: TListBox;
+    procedure Button1Click(Sender: TObject);
   private
     { Private êÈåæ }
   public
@@ -20,5 +24,14 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if Edit1.Text = '' then
+    Exit;
+
+  ListBox1.Items.Add(Edit1.Text);
+  Edit1.Text := '';
+end;
 
 end.
