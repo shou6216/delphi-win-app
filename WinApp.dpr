829e2dd3d234319563871ad489053aa7aa57898f
s-yoshino <shou-y6216@yoshinoda.com>
2018-09-08 12:45:58 +0900

    [update] Unit3とUnit2を追加

--- WinApp.dpr
+++ WinApp.dpr
@@ -2,7 +2,9 @@ program WinApp;
 
 uses
   Vcl.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  Unit1 in 'Unit1.pas' {Form1},
+  Unit2 in 'Unit2.pas' {Form2},
+  Unit3 in 'Unit3.pas' {Form3};
 
 {$R *.res}
 
@@ -10,5 +12,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm3, Form3);
   Application.Run;
 end.
